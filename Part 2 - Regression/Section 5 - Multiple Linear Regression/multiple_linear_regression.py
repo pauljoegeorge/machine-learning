b68668b02d135b2098311e06ceca2b61c1659f26
pauljoegeorge <pauljoegeorge@gmail.com>
2019-04-17 10:26:43 +0900

    multiple linear regression completed

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -15,15 +15,6 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values  #takes all columns except the last one (independent variables)
 Y = dataset.iloc[:, 4].values   #index value from 0
 
-# taking care of missing data
-"""
-# replace Nan values by mean value
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-"""
-
 # Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 #convert countries to 0,1,2
@@ -53,7 +44,9 @@ regressor.fit(X_train, Y_train)
 #predict the test set results (profit)
 y_pred = regressor.predict(X_test)
 
-
+"""
+# Manual implementation
+"""
 #currently all columns (relavant and least relavant) are used. but eliminating least relavant columns will optimize the prediction
 #building the optimal model using Backward Elimination
 import statsmodels.formula.api as sm
@@ -71,24 +64,47 @@ regressor_OLS.summary()
 X_opt = X[:, [0,1,3,4,5]]
 regressor_OLS = sm.OLS(endog= Y, exog= X_opt).fit()
 regressor_OLS.summary()
-
+X_opt = X[:, [0,3,4,5]]
+regressor_OLS = sm.OLS(endog= Y, exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0,3,5]]
+regressor_OLS = sm.OLS(endog= Y, exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0,3]]
+regressor_OLS = sm.OLS(endog= Y, exog= X_opt).fit()
+regressor_OLS.summary()
 
 """
-#visualise the `train set` results
-plt.scatter(X_train, Y_train, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+# Automatic implementation
 
-plt.title('Company vs Growth (Training Set)')
-plt.xlabel('Company growth parameters')
-plt.ylabel('Profit')
-plt.show()
-
-#visualise the `test set` results
-plt.scatter(X_test, Y_test, color = 'red')
-plt.plot(X_train, regressor.predict(X_train), color = 'blue')
-
-plt.title('Company vs Growth (Test Set)')
-plt.xlabel('Company growth paramters')
-plt.ylabel('Profit')
-plt.show()
+import statsmodels.formula.api as sm
+def backwardElimination(x, SL):
+    numVars = len(x[0])
+    temp = np.zeros((50,6)).astype(int)
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        adjR_before = regressor_OLS.rsquared_adj.astype(float)
+        if maxVar > SL:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    temp[:,j] = x[:, j]
+                    x = np.delete(x, j, 1)
+                    tmp_regressor = sm.OLS(y, x).fit()
+                    adjR_after = tmp_regressor.rsquared_adj.astype(float)
+                    if (adjR_before >= adjR_after):
+                        x_rollback = np.hstack((x, temp[:,[0,j]]))
+                        x_rollback = np.delete(x_rollback, j, 1)
+                        print (regressor_OLS.summary())
+                        return x_rollback
+                    else:
+                        continue
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
 """
+
+
