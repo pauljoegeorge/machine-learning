48ec36b5b373483e3856f95a29a4cd21cf4c6d63
pauljoegeorge <pauljoegeorge@gmail.com>
2019-08-22 09:02:56 +0900

    cnn is progress, compiled

--- Part 8 - Deep Learning/Convolutional_Neural_Networks/cnn.py
+++ Part 8 - Deep Learning/Convolutional_Neural_Networks/cnn.py
@@ -14,4 +14,41 @@ from keras.layers import Dense   # dense layer
 # init CNN
 classifier = Sequential()
 
-# adding convolutional layer (Layer 1)
+"""
+# STEP1: adding convolutional layer (Layer 1)
+"""
+classifier.add(Convolution2D(32,3,3, input_shape=(64, 64, 3), activation = 'relu'))  #64 feature map , 3 rows and 3 cols for feature detector
+# we are working on cpu not gpu , so feature map has to be low value.  
+# input shape for colored image (3,256,256) => 3 channel (RGB), size 256 pixel each (2D)
+# since we are using tensorflow backend input shape input will in (64, 64, 3) format
+# if using Theano order will be reverse
+# Add activation layer to eliminate non-linearity
+
+"""
+# STEP 2 -> POOLING
+"""
+# Slide with stride of 2 (not one ) and will take the max value
+# in step 1 stride applied is 1 
+# Why pooling? reduce the size by 75% without removing the unique features
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+"""
+# STEP 3 -> FLATTENING
+"""
+# create a single vector using pooling output as input
+classifier.add(Flatten())
+
+"""
+# STEP 4 -> FULLY CONNECTED LAYER add(input layer, hidden layer, output layer)
+"""
+classifier.add(Dense(128, kernel_initializer="uniform", activation = 'relu')) # hidden layer
+# output_dim = no of hidden layer nodes
+classifier.add(Dense(1, kernel_initializer="uniform", activation = 'sigmoid')) # for output layer-> sigmoid
+# expecting just one output, cat or dog. so output_dim is 1
+
+
+"""
+STEP 5 -> COMPILE WHOLE MODEL
+"""
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics=['accuracy'])
+# loss -> since we have binary outcome -> binary cross entropy
